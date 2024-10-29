c484af545887d8961c1deede26146d128bd9f252
Brendan-duPlooy <u23605376@tuks.co.za>
2024-10-29 16:16:05 +0200

    All of Brendans Code beside Iterator
    - Buildings
    - Government

---
+++ Buildings.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+#include <list>
+#include "Buildings.h"
+
+void Buildings::add(Buildings* component) 
+{
+    throw std::logic_error("Operation not supported");
+}
+
+void Buildings::remove(Buildings* component) 
+{
+    throw std::logic_error("Operation not supported");
+}
+
+string Buildings::getType() 
+{
+    throw std::logic_error("Operation not supported");
+}
+
+int Buildings::getPrice() 
+{
+    throw std::logic_error("Operation not supported");
+}
+
+void Buildings::setPrice(int p) 
+{
+    throw std::logic_error("Operation not supported");
+}
