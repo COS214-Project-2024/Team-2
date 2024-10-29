5fc95373ceba3728745070ee9a27fac290ec46a2
NotEuanB <euanbotha@gmail.com>
2024-10-29 15:44:23 +0200

    steel and concretee

--- Facilities/Concrete.h
+++ Facilities/Concrete.h
@@ -1,9 +1,19 @@
 #include "Materials.h"
+#include "StateOfResources.h"
 
 class Concrete : public Materials {
-    private:
-        int quantity;
-        StateOfResources state;
-    public:
-        void useResource(int quantity);
+private:
+    int quantity;
+    StateOfResources* state;
+    StateOfResources* fullState;
+    StateOfResources* emptyState;
+    StateOfResources* normalState;
+
+public:
+    Concrete(StateOfResources* initialState, int initialQuantity);
+    void useResource(int quantity) override;
+    void generateResource(int quantity) override;
+    void setState(StateOfResources* newState);
+    int getQuantity() const override;
+    int getMaxCapacity() const override;
 };
