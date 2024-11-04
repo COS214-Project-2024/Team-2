a7ccc42178bf225a27e7ec5f7e3089e7b4de6fff
Hannah_Koorbanally <168213067+Hannah1704@users.noreply.github.com>
2024-11-04 14:58:36 +0200

    Delete Code directory

--- Code/ConcreteCitizenFactory.cpp
+++
@@ -1,5 +0,0 @@
-#include "ConcreteCitizenFactory.h"
-
-ConcreteCitizen* ConcreteCitizenFactory::createCitizen(BuildingsUnit* buildings, Government* government){
-    return new ConcreteCitizen(buildings, government);
-}
