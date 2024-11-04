f634b55e639837b26e2037ebed50d7407cd0137f
Brendan-duPlooy <u23605376@tuks.co.za>
2024-11-04 21:46:56 +0200

    Delete Utilities directory

--- Utilities/MaterialFactory.cpp
+++
@@ -1,9 +0,0 @@
-#include "MaterialFactory.h"
-
-void MaterialFactory::handleRequest(Government* govern)
-{
-    govern->useResource("steel");
-    govern->useResource("wood");
-    govern->useResource("concrete");
-    Utilities::handleRequest(govern);
-}
--- Utilities/PowerPlant.cpp
+++
@@ -1,7 +0,0 @@
-#include "PowerPlant.h"
-
-void PowerPlant::handleRequest(Government* govern) 
-{
-    govern->useResource("energy");
-    Utilities::handleRequest(govern);
-}
