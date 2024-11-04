ae90bb73271b844bfcbb0d5a1544c8dd05c8b4ae
kiara <u23530996@tuks.co.za>
2024-11-04 17:31:00 +0200

    Delete Resources directory

--- Resources/FullState.h
+++
@@ -1,7 +0,0 @@
-#include "StateOfResources.h"
-
-class FullState : public StateOfResources {
-    public:
-        void useResource(int quantity) override;
-        void generateResource(int quantity) override;
-};
--- Resources/NormalState.h
+++
@@ -1,7 +0,0 @@
-#include "StateOfResources.h"
-
-class NormalState : public StateOfResources {
-    public:
-        void useResource(int quantity) override;
-        void generateResource(int quantity) override;
-};
